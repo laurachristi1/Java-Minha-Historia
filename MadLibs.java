33f9d0d59aa226017b52df64fbf0610ac9f1a7bd
laurachristi1 <126782238+laurachristi1@users.noreply.github.com>
2023-04-17 20:20:03 -0300

    Java-Minha História

---
+++ MadLibs.java
@@ -0,0 +1,27 @@
+public class MadLibs{
+    //Esse programa se chama "Minha História"
+    //Autora: Laura Christi
+    //Data: 15/12/2045
+
+    public static void main (String[]args){
+
+    String name1 = "Adriana";
+    String adjective1 = " very nice";
+    String adjective2 = "good"; 
+    String adjective3 = "hard";
+    String verb1 = "do";
+    String noun1 = "people";
+    String noun2 = "camera";
+    String noun3 = "day";
+    String noun4 = "things";
+    String noun5 = "part";
+    String noun6 = "actions";
+    String name2 = "João";
+    int number = 2046;
+    String place = "Marte";
+
+    String story = "This morning "+name1+" woke up feeling "+adjective1+". It is going to be a "+adjective2+" day! 'Outside, a bunch of "+noun1+"s were prottesting to keep "+noun2+" in stores. They began to "+verb1+" to the rhythm of the "+noun3+", which made all the "+noun4+"s very "+adjective3+". Concerned, "+name1+" texted "+name2+", who flew "+name1+" to "+place+" and dropped "+name1+" in a puddle of frozen "+noun5+". "+name1+" woke up in the year "+number+", in a word where "+noun6+"s ruled the word.";
+
+    System.out.println(story);
+ }
+}
